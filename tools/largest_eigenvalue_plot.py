d57cdd8b11c33506e88405b8ede98454fb3b6302
Jason Moore <moorepants@gmail.com>
2011-05-16 15:35:37 -0700

    added script for the largest eigenvalue plot of the bikes in the IEEE paper

---
+++ tools/largest_eigenvalue_plot.py
@@ -0,0 +1,24 @@
+import BicycleParameters as bp
+
+bikeNames = ['Browserins', 'Browser', 'Pista',
+             'Fisher', 'Yellow', 'Yellowrev']
+
+colors = ['k'] * 3 + ['gray'] * 3
+
+linestyles = ['-', ':', '--'] * 2 
+
+bikes = []
+
+for name in bikeNames:
+    bikes.append(bp.Bicycle(name, pathToBicycles='../bicycles'))
+
+speeds = bp.np.linspace(0., 10., num=100)
+
+bp.plot_eigenvalues(bikes, speeds, colors=colors,
+                    linestyles=linestyles, largest=True)
+
+bp.plt.vlines([2.5, 5., 7.], -0.5, 4.)
+
+bp.plt.savefig('../plots/largest.png')
+
+bp.plt.show()
