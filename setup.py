20c0e4f888c82bf38dd11d0438f60a4070d491b6
Jason Moore <moorepants@gmail.com>
2011-06-29 13:57:22 -0700

    added a setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='BicycleParameters',
+    version='0.1.0',
+    author='Jason Keith Moore',
+    author_email='moorepants@gmail.com',
+    packages=['bicycleparameters', 'bicycleparameters.test'],
+    scripts=['bin/create_bicycle_files.py','bin/largest_eigenvalue_plot.py'],
+    url='http://pypi.python.org/pypi/BicycleParameters/',
+    license='LICENSE.txt',
+    description='Generates and manipulates the physical parameters of a bicycle.',
+    long_description=open('README.txt').read(),
+)
