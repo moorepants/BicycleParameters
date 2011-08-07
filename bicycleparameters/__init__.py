478bb08f23ae51fa0489cf9b599a6f980b819ff6
Jason Moore <moorepants@gmail.com>
2011-08-06 23:21:54 -0700

    added a version number in the package init file

--- bicycleparameters/__init__.py
+++ bicycleparameters/__init__.py
@@ -3,3 +3,7 @@ from bicycleparameters import *
 # remove the bicycleparameters and inertia modules because we already imported
 # all the stuff from it
 del bicycleparameters, inertia
+
+# specify the version for the package
+__version_info__ = (0, 1, 0)
+__version__ = '.'.join(map(str, __version_info__))
