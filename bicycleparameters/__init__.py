ed149e539e4cd1bcef12766842d28fa66720966c
Jason Moore <moorepants@gmail.com>
2011-08-06 23:06:58 -0700

    fixed the files so that it imports just the functionality from bicycleparameters

---
+++ bicycleparameters/__init__.py
@@ -0,0 +1,5 @@
+# import all the classes and functions from the module
+from bicycleparameters import *
+# remove the bicycleparameters and inertia modules because we already imported
+# all the stuff from it
+del bicycleparameters, inertia
