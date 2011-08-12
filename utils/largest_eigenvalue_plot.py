e90e7ad38be55113bc88082f79fbfb4cdf8bbade
Jason Moore <moorepants@gmail.com>
2011-08-12 16:17:08 -0700

    updated largest_eig... to work with new syntax

--- utils/largest_eigenvalue_plot.py
+++ utils/largest_eigenvalue_plot.py
@@ -1,4 +1,5 @@
-import BicycleParameters as bp
+import bicycleparameters as bp
+import numpy as np
 
 bikeNames = ['Browserins', 'Browser', 'Pista',
              'Fisher', 'Yellow', 'Yellowrev']
@@ -10,15 +11,15 @@ linestyles = ['-', '-.', '--'] * 2
 bikes = []
 
 for name in bikeNames:
-    bikes.append(bp.Bicycle(name, pathToBicycles='../bicycles'))
+    bikes.append(bp.Bicycle(name, pathToData='../data'))
 
-speeds = bp.np.linspace(0., 10., num=100)
+speeds = np.linspace(0., 10., num=100)
 
 bp.plot_eigenvalues(bikes, speeds, colors=colors,
-                    linestyles=linestyles, largest=True)
+                    linestyles=linestyles), largest=True)
 
 bp.plt.vlines([2.5, 5., 7.], -0.5, 4.)
 
-bp.plt.savefig('../plots/largest.png')
+#bp.plt.savefig('../plots/largest.png')
 
 bp.plt.show()
