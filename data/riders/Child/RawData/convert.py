ee554b55511e521f7eee3af547d067fd1d30d1a6
Jason Moore <moorepants@gmail.com>
2011-12-14 10:03:11 -0800

    Added the input file for a child sized person.

---
+++ data/riders/Child/RawData/convert.py
@@ -0,0 +1,17 @@
+# this simply scales Charlie's input file with respect to the average height of
+# a five year old male.
+scale = 1.016 / 1.758;
+
+f = open('CharlieYeadonMeas.txt', 'r')
+n = open('ChildYeadonMeasScaled.txt', 'w')
+
+for line in f:
+    if line.startswith('L'):
+        var, val = line.strip().split('=')
+        scaled = scale * float(val)
+        n.write(var + '=' + str(scaled) + '\n')
+    else:
+        n.write(line)
+
+f.close()
+n.close()
