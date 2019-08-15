547b9386538814c6c44dd43747d7c6a07ae4b629
Jason K. Moore <moorepants@gmail.com>
2019-08-15 10:33:59 -0700

    Deduplicated some code and use mpl color cycler.

--- bicycleparameters/parameter_sets.py
+++ bicycleparameters/parameter_sets.py
@@ -23,7 +23,38 @@ def _com_symbol(ax, center, radius, color='b', label=None):
     return ax
 
 
+def _plot_geometry_from_benchmark(ax, parameters, show_steer_axis=True):
+
+    p = parameters
+
+    # plot the ground line
+    ax.axhline(0.0, color='black')
+
+    # plot the rear wheel
+    c = patches.Circle((0., -p['rR']), radius=p['rR'],
+                       linewidth=2, fill=False)
+    ax.add_patch(c)
+
+    # plot the front wheel
+    c = patches.Circle((p['w'], -p['rF']), radius=p['rF'],
+                       linewidth=2, fill=False)
+    ax.add_patch(c)
+
+    # plot the fundamental bike (Moore2012 depiction)
+    deex, deez = fundamental_geometry_plot_data(p)
+    ax.plot(deex, deez, color='k', linewidth=2)
+
+    if show_steer_axis:
+        # plot the steer axis
+        dx3 = deex[2] - deez[2] * (deex[2] - deex[1]) / (-deez[1] + deez[2])
+        ax.plot([deex[2], dx3],  [deez[2], 0.], 'k--')
+
+    return ax
+
+
 class ParameterSet(object):
+
+    parameterization = None
 
     def _check_parameters(self, parameters):
         """Ensures that each parameter in par_strings is present in parameters
@@ -39,15 +70,41 @@ class ParameterSet(object):
     def __init__(self, par_dict):
         self._check_parameters(par_dict)
 
+    def _generate_body_colors(self):
+        prop_cycle = plt.rcParams['axes.prop_cycle']
+        colors = prop_cycle.by_key()['color']
+        self.body_colors = {}
+        for i, b in enumerate(self.body_labels):
+            self.body_colors[b] = colors[i]
+
+    def _invert_yaxis(self, ax):
+        if not ax.yaxis_inverted():
+            ax.invert_yaxis()
+        return ax
+
+    def _finalize_plot(self, ax):
+        ax = self._invert_yaxis(ax)
+        ax.set_aspect('equal')
+        ax.set_xlabel(r'$x$ [m]')
+        ax.set_ylabel(r'$z$ [m]')
+
     def to_yaml(self, fname):
         """Writes parameters to file in the YAML format."""
+        data = {'values': self.parameters,
+                'parameterization': self.parameterization,
+                'parameters': '',
+                'description': '',
+                'rider': None,
+                }
         with open(fname, 'w') as f:
-            yaml.dump(self.parameters, f)
+            yaml.dump(data, f)
 
 
 class BenchmarkParameterSet(ParameterSet):
     """Represents the parameters of the benchmark bicycle presented in
     Meijaard2007."""
+
+    parameterization = 'benchmark'
 
     # maps "Python" string to LaTeX version
     par_strings = {
@@ -95,11 +152,7 @@ class BenchmarkParameterSet(ParameterSet):
         self.parameters = parameters
         self.includes_rider = includes_rider
         self.body_labels = ['B', 'F', 'H', 'R']
-
-        cmap = plt.get_cmap('gist_rainbow')
-        self.body_colors = {}
-        for i, part in enumerate(self.body_labels):
-            self.body_colors[part] = cmap(1. * i / len(self.body_labels))
+        self._generate_body_colors()
 
     def _calc_derived_params(self):
         p = self.parameters
@@ -119,12 +172,6 @@ class BenchmarkParameterSet(ParameterSet):
         pext['zR'] = -p['rR']
 
         return pext
-
-    def _finalize_plot(self, ax):
-        ax = self._invert_yaxis(ax)
-        ax.set_aspect('equal')
-        ax.set_xlabel(r'$x$')
-        ax.set_ylabel(r'$z$')
 
     def form_mass_center_vector(self, body):
         """Returns a (3, 1) NumPy array representing the vector to the mass
@@ -176,120 +223,25 @@ class BenchmarkParameterSet(ParameterSet):
         if ax is None:
             fig, ax = plt.subplots()
 
-        # plot the ground line
-        ax.axhline(0.0, color='black')
+        ax = _plot_geometry_from_benchmark(ax, p,
+                                           show_steer_axis=show_steer_axis)
 
-        # plot the rear wheel
-        c = patches.Circle((0., p['rR']), radius=p['rR'],
-                           linewidth=2, fill=False)
-        ax.add_patch(c)
-
-        # plot the front wheel
-        c = patches.Circle((p['w'], p['rF']), radius=p['rF'],
-                           linewidth=2, fill=False)
-        ax.add_patch(c)
-
-        # plot the fundamental bike
-        deex, deez = fundamental_geometry_plot_data(p)
-        ax.plot(deex, -deez, 'k', linewidth=2)
-
-        if show_steer_axis:
-            # plot the steer axis
-            dx3 = deex[2] + deez[2] * (deex[2] - deex[1]) / (deez[1] - deez[2])
-            ax.plot([deex[2], dx3],  [-deez[2], 0.], 'k--')
-
-        ax.set_aspect('equal')
+        self._finalize_plot(ax)
 
         return ax
-
-    def plot_principal_radii_of_gyration(self, ax=None):
-
-        p = self.parameters
-
-        if ax is None:
-            fig, ax = plt.subplots()
-
-        def plot_titled_radii(b):
-
-            color = self.body_colors[b]
-
-            x = 'x{}'.format(b)
-            z = 'z{}'.format(b)
-
-            angle_max, radius_max, angle_min, radius_min = \
-                self._planar_principal_radii_of_gyration(b)
-
-            ax.plot([p[x], p[x] + np.cos(angle_max) * radius_min],
-                    [-p[z], -p[z] - np.sin(angle_max) * radius_min],
-                    color=color)
-            ax.plot([p[x], p[x] - np.cos(angle_max) * radius_min],
-                    [-p[z], -p[z] + np.sin(angle_max) * radius_min],
-                    color=color)
-
-            ax.plot([p[x], p[x] + np.cos(angle_min) * radius_max],
-                    [-p[z], -p[z] - np.sin(angle_min) * radius_max],
-                    color=color)
-            ax.plot([p[x], p[x] - np.cos(angle_min) * radius_max],
-                    [-p[z], -p[z] + np.sin(angle_min) * radius_max],
-                    color=color)
-
-            kyy = np.sqrt(p['I{}yy'.format(b)] / p['m{}'.format(b)])
-
-            c = patches.Circle((p[x], -p[z]), radius=kyy, fill=False,
-                               color=color)
-            ax.add_patch(c)
-
-        plot_titled_radii('H')
-        plot_titled_radii('B')
-
-        kRxx = np.sqrt(p['IRxx'] / p['mR'])
-        kRyy = np.sqrt(p['IRyy'] / p['mR'])
-        ax.plot([-kRxx, kRxx], [p['rR'], p['rR']], color=self.body_colors['R'])
-        ax.plot([0.0, 0.0], [p['rR'] - kRxx, p['rR'] + kRxx],
-                color=self.body_colors['R'])
-        c = patches.Circle((0., p['rR']), radius=kRyy,
-                           fill=False, color=self.body_colors['R'])
-        ax.add_patch(c)
-
-        kFxx = np.sqrt(p['IFxx'] / p['mF'])
-        kFyy = np.sqrt(p['IFyy'] / p['mF'])
-        ax.plot([p['w']-kFxx, p['w']+kFxx], [p['rF'], p['rF']],
-                color=self.body_colors['F'])
-        ax.plot([p['w'], p['w']], [p['rF'] - kFxx, p['rF'] + kFxx],
-                color=self.body_colors['F'])
-        c = patches.Circle((p['w'], p['rF']), radius=kFyy,
-                           fill=False, color=self.body_colors['F'])
-        ax.add_patch(c)
-
-        ax.set_aspect('equal')
-
-        return ax
-
-    def _planar_principal_radii_of_gyration(self, body):
-
-        p = self.parameters.copy()
-        p.update(self._calc_derived_params())
-        b = body
-
-        IH = np.array([[p['I{}xx'.format(b)], p['I{}xz'.format(b)]],
-                       [p['I{}xz'.format(b)], p['I{}zz'.format(b)]]])
-
-        evals, evecs = np.linalg.eig(IH)
-
-        idxs = np.argsort(evals)
-
-        evals = evals[idxs]
-        evecs = evecs[:, idxs]
-
-        angle_to_max = np.arctan2(evecs[1, 0], evecs[0, 0])
-        angle_to_min = np.arctan2(evecs[1, 1], evecs[0, 1])
-
-        kHmax = np.sqrt(evals[0] / p['m{}'.format(b)])
-        kHmin = np.sqrt(evals[1] / p['m{}'.format(b)])
-
-        return angle_to_max, kHmax, angle_to_min, kHmin
 
     def plot_mass_centers(self, bodies=None, ax=None):
+        """Returns a matplotlib axes with mass center indicators for each body.
+
+        Parameters
+        ==========
+        bodies: list of strings, optional
+            A subset of the strings present in the class attribute
+            ``body_labels``.
+        ax: matplotlib Axes, optional
+            An axes to plot on.
+
+        """
 
         if ax is None:
             fig, ax = plt.subplots()
@@ -298,7 +250,7 @@ class BenchmarkParameterSet(ParameterSet):
             bodies = self.body_labels
 
         for b in bodies:
-            ax = self.plot_body_mass_center(b, ax=ax, label=b)
+            ax = self.plot_body_mass_center(b, ax=ax)
 
         return ax
 
@@ -324,54 +276,42 @@ class BenchmarkParameterSet(ParameterSet):
         # mass center
         x = p['x{}'.format(b)]
         z = p['z{}'.format(b)]
-        radius = p['w'] / 30
-        ax = _com_symbol(ax, (x, z), radius, color=self.body_colors[b])
+        radius = p['w'] / 10
+        ax = _com_symbol(ax, (x, z), radius, color=self.body_colors[b], label=b)
 
         self._finalize_plot(ax)
 
         return ax
 
-    def plot_mass_centers(self, ax=None):
-        """Returns a Matplotlib axes with each of the four mass centers marked.
+    def _planar_principal_radii_of_gyration(self, body):
 
-        Parameters
-        ==========
-        ax : AxesSubplot, optional
-            An axes to draw on, otherwise one is created.
+        p = self.parameters.copy()
+        p.update(self._calc_derived_params())
 
-        """
+        b = body
 
-        p = self.parameters
+        IH = self.form_inertia_tensor(body)
+        # remove the Y row and column
+        IH_xz_plane = np.delete(np.delete(IH, 1, axis=1), 1, axis=0)
 
-        if ax is None:
-            fig, ax = plt.subplots()
+        evals, evecs = np.linalg.eig(IH_xz_plane)
 
-        # radius of the CoM symbol
-        sRad = 0.03
+        idxs = np.argsort(evals)
 
-        # front wheel CoM
-        ax = _com_symbol(ax, (p['w'], p['rF']), sRad,
-                        color=self.body_colors['F'])
-        ax.text(p['w'] + sRad, p['rF'] + sRad, 'F')
-        # rear wheel CoM
-        ax = _com_symbol(ax, (0., p['rR']), sRad, color=self.body_colors['R'])
-        ax.text(0. + sRad, p['rR'] + sRad, 'R')
-        # front frame CoM
-        ax = _com_symbol(ax, (p['xH'], -p['zH']), sRad,
-                        color=self.body_colors['H'])
-        ax.text(p['xH'] + sRad, -p['zH'] + sRad, 'H')
-        # rear frame (and rider) CoM
-        ax = _com_symbol(ax, (p['xB'], -p['zB']), sRad,
-                        color=self.body_colors['B'])
-        s = 'B (frame + rider)'
-        ax.text(p['xB'] + sRad, -p['zB'] + sRad,
-                s if self.includes_rider else 'B')
+        evals = evals[idxs]
+        evecs = evecs[:, idxs]
 
-        ax.set_aspect('equal')
+        angle_to_max = np.arctan2(evecs[1, 0], evecs[0, 0])
 
-        return ax
+        kmax = np.sqrt(evals[0] / p['m{}'.format(b)])
+        kmin = np.sqrt(evals[1] / p['m{}'.format(b)])
+        kyy = np.sqrt(p['I{}yy'.format(b)] / p['m{}'.format(b)])
 
-    def plot_body_inertia_ellipsoid(self, b, ax=None):
+        return kmax, kmin, kyy, angle_to_max
+
+    def plot_body_principal_radii_of_gyration(self, b, ax=None):
+        """Returns a matplotlib axes with lines and a circle that indicate the
+        principal radii of gyration of the specified body."""
 
         if ax is None:
             fig, ax = plt.subplots()
@@ -379,21 +319,69 @@ class BenchmarkParameterSet(ParameterSet):
         p = self.parameters.copy()
         p.update(self._calc_derived_params())
 
-        angle_max, radius_max, angle_min, radius_min = \
-            self._planar_principal_radii_of_gyration(b)
-        kyy = np.sqrt(p['I{}yy'.format(b)] / p['m{}'.format(b)])
+        x = p['x{}'.format(b)]
+        z = p['z{}'.format(b)]
+        kaa, kbb, kyy, alpha = self._planar_principal_radii_of_gyration(b)
 
-        width = np.sqrt(5/2*(-radius_max**2 + kyy**2 + radius_min**2))
-        height = np.sqrt(5/2*(radius_max**2 + kyy**2 - radius_min**2))
+        c = patches.Circle((x, z), radius=kyy, fill=False,
+                           color=self.body_colors[b], linestyle='--')
+        ax.add_patch(c)
+
+        # NOTE : -alpha is required because we are mapping the xz axes to a new
+        # planar drawing grid which is x and y with z pointing out of the
+        # screen
+        ax.plot([x - kbb*np.cos(-alpha), x + kbb*np.cos(-alpha)],
+                [z - kbb*np.sin(-alpha), z + kbb*np.sin(-alpha)],
+                color=self.body_colors[b], linestyle="--")
+
+        ax.plot([x - kaa*np.cos(-alpha - np.pi/2),
+                 x + kaa*np.cos(-alpha - np.pi/2)],
+                [z - kaa*np.sin(-alpha - np.pi/2),
+                 z + kaa*np.sin(-alpha - np.pi/2)],
+                color=self.body_colors[b], linestyle="--")
+
+        self._finalize_plot(ax)
+
+        return ax
+
+    def plot_principal_radii_of_gyration(self, bodies=None, ax=None):
+
+        if ax is None:
+            fig, ax = plt.subplots()
+
+        if bodies is None:
+            bodies = self.body_labels
+
+        for b in bodies:
+            ax = self.plot_body_principal_radii_of_gyration(b, ax=ax)
+
+        return ax
+
+    def plot_body_principal_inertia_ellipsoid(self, b, ax=None):
+        """Returns a matplotlib axes with an ellipse that respresnts the XZ
+        plane view of a constant density ellipsoid which has the same principal
+        moments and axes of inertia as the body."""
+
+        if ax is None:
+            fig, ax = plt.subplots()
+
+        p = self.parameters.copy()
+        p.update(self._calc_derived_params())
+
+        kaa, kbb, kyy, alpha = self._planar_principal_radii_of_gyration(b)
+
+        width = np.sqrt(5/2*(-kaa**2 + kyy**2 + kbb**2))
+        height = np.sqrt(5/2*(kaa**2 + kyy**2 - kbb**2))
+
         ellipse = patches.Ellipse((p['x{}'.format(b)],
-                                  -p['z{}'.format(b)]), width, height,
-                                  angle=-np.rad2deg(angle_max), fill=False,
+                                  p['z{}'.format(b)]), width, height,
+                                  angle=-np.rad2deg(alpha), fill=False,
                                   color=self.body_colors[b])
         ax.add_patch(ellipse)
 
         return ax
 
-    def plot_inertia_ellipsoids(self, ax=None):
+    def plot_principal_inertia_ellipsoids(self, bodies=None, ax=None):
         """Returns a Matplotlib axes with 2D representations of 3D solid
         uniform ellipsoids that have the same inertia as the body.
 
@@ -403,29 +391,14 @@ class BenchmarkParameterSet(ParameterSet):
             An axes to draw on, otherwise one is created.
 
         """
-        p = self.parameters.copy()
-        p.update(self._calc_derived_params())
-
         if ax is None:
             fig, ax = plt.subplots()
 
-        def add_ellipse(b):
-            angle_max, radius_max, angle_min, radius_min = \
-                self._planar_principal_radii_of_gyration(b)
-            kyy = np.sqrt(p['I{}yy'.format(b)] / p['m{}'.format(b)])
+        if bodies is None:
+            bodies = self.body_labels
 
-            width = np.sqrt(5/2*(-radius_max**2 + kyy**2 + radius_min**2))
-            height = np.sqrt(5/2*(radius_max**2 + kyy**2 - radius_min**2))
-            ellipse = patches.Ellipse((p['x{}'.format(b)],
-                                       -p['z{}'.format(b)]), width, height,
-                                      angle=-np.rad2deg(angle_max), fill=False,
-                                      color=self.body_colors[b], alpha=0.25)
-            ax.add_patch(ellipse)
-
-        add_ellipse('B')
-        add_ellipse('F')
-        add_ellipse('H')
-        add_ellipse('R')
+        for b in bodies:
+            ax = self.plot_body_principal_inertia_ellipsoid(b, ax=ax)
 
         return ax
 
@@ -489,7 +462,7 @@ class PrincipalParameterSet(ParameterSet):
         'zP': r'z_P',
     }
 
-    def __init__(self, parameters, body_colors=None):
+    def __init__(self, parameters):
         """Initializes a parameter set based on Moore2019.
 
         Parameters
@@ -504,13 +477,7 @@ class PrincipalParameterSet(ParameterSet):
         self.parameters = parameters
 
         self.body_labels = ['D', 'F', 'H', 'P', 'R']
-
-        if body_colors is None:
-            self.body_colors = {'D': 'red',
-                                'P': 'blue',
-                                'R': 'orange',
-                                'F': 'green',
-                                'H': 'purple', }
+        self._generate_body_colors()
 
     def _calc_derived_params(self):
         p = self.parameters
@@ -532,16 +499,31 @@ class PrincipalParameterSet(ParameterSet):
 
         return pext
 
-    def _invert_yaxis(self, ax):
-        if not ax.yaxis_inverted():
-            ax.invert_yaxis()
-        return ax
+    def plot_geometry(self, show_steer_axis=True, ax=None):
+        """Returns a matplotlib axes with the simplest drawing of the bicycle's
+        geometry.
 
-    def _finalize_plot(self, ax):
-        ax = self._invert_yaxis(ax)
-        ax.set_aspect('equal')
-        ax.set_xlabel(r'$x$')
-        ax.set_ylabel(r'$z$')
+        Parameters
+        ==========
+        show_steer_axis : boolean
+            If true, a dotted line will be plotted along the steer axis from
+            the front wheel center to the ground.
+        ax : AxesSubplot, optional
+            An axes to draw on, otherwise one is created.
+
+        """
+
+        p = self.parameters
+
+        if ax is None:
+            fig, ax = plt.subplots()
+
+        ax = _plot_geometry_from_benchmark(ax, p,
+                                           show_steer_axis=show_steer_axis)
+
+        self._finalize_plot(ax)
+
+        return ax
 
     def plot_person_diamond(self, show_cross=False, ax=None):
         """Plots a diamond that represents the approximate person's physical
@@ -689,51 +671,6 @@ class PrincipalParameterSet(ParameterSet):
 
         return ax
 
-    def plot_geometry(self, show_steer_axis=True, ax=None):
-        """Returns a matplotlib axes with the simplest drawing of the bicycle's
-        geometry.
-
-        Parameters
-        ==========
-        show_steer_axis : boolean
-            If true, a dotted line will be plotted along the steer axis from
-            the front wheel center to the ground.
-        ax : AxesSubplot, optional
-            An axes to draw on, otherwise one is created.
-
-        """
-
-        p = self.parameters
-
-        if ax is None:
-            fig, ax = plt.subplots()
-
-        # plot the ground line
-        ax.axhline(0.0, color='black')
-
-        # plot the rear wheel
-        c = patches.Circle((0., -p['rR']), radius=p['rR'],
-                           linewidth=2, fill=False)
-        ax.add_patch(c)
-
-        # plot the front wheel
-        c = patches.Circle((p['w'], -p['rF']), radius=p['rF'],
-                           linewidth=2, fill=False)
-        ax.add_patch(c)
-
-        # plot the fundamental bike
-        deex, deez = fundamental_geometry_plot_data(p)
-        ax.plot(deex, deez, 'k', linewidth=2)
-
-        if show_steer_axis:
-            # plot the steer axis
-            dx3 = deex[2] - deez[2] * (deex[2] - deex[1]) / (-deez[1] + deez[2])
-            ax.plot([deex[2], dx3],  [deez[2], 0.], 'k--')
-
-        self._finalize_plot(ax)
-
-        return ax
-
     def plot_body_principal_inertia_ellipsoid(self, b, ax=None):
         """Returns a matplotlib axes with an ellipse that respresnts the XZ
         plane view of a constant density ellipsoid which has the same principal
@@ -760,5 +697,7 @@ class PrincipalParameterSet(ParameterSet):
                                   angle=-np.rad2deg(alpha), fill=False,
                                   color=self.body_colors[b])
         ax.add_patch(ellipse)
+
+        self._finalize_plot(ax)
 
         return ax
