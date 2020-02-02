c0b7dcdbe62a625b42834b5424ead90511c2c476
Noah Sanders <noasanders@ucdavis.edu>
2020-02-02 12:21:51 -0800

    first instance of stable dataplot element

--- plotdrawer.py
+++ plotdrawer.py
@@ -4,12 +4,16 @@ import numpy as np
 
 speeds = np.linspace(0, 10, num=100)
 
-### Clunkily draws and saves default plots ###
-def draw_plot():
+    # Clunkily draws and saves default plots 
+def save_plot():
     bike = bp.Bicycle('Benchmark', pathToData=os.getcwd()+'\\data')  # Alter Bicycle name
     #bike.add_rider('Jason', reCalc=True)
     plot = bike.plot_eigenvalues_vs_speed(speeds, show=False)
     plot.savefig(os.getcwd()+'\\assets\\eigen-plots\\defaults\\Benchmark.png')  # Alter png name
+
+def draw_plot():
+    bike = bp.Bicycle('', pathToData=os.getcwd()+'\\data')
+    plot = bike.plot_eigenvalues_vs_speed(speeds, show=True)
 draw_plot()
 
 '''
