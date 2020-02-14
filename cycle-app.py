9dcac39c5790d2833ca6c2f7bd457f7a87046b12
Noah Sanders <noasanders@ucdavis.edu>
2020-02-13 19:34:28 -0800

    formats input data for wheel table display

--- cycle-app.py
+++ cycle-app.py
@@ -5,7 +5,9 @@ import dash_table as tbl
 from dash.dependencies import Input, Output, State
 from dash.exceptions import PreventUpdate
 
+from collections import OrderedDict
 import os
+
 import bicycleparameters as bp
 import pandas as pd
 import numpy as np 
@@ -17,9 +19,23 @@ from bicycleparameters import parameter_sets as ps
     # Initializes bike data as Bicycle and retrieves its parameters
 def new_par(bike_name):
     bike = bp.Bicycle(bike_name, pathToData=os.getcwd()+'\\data')
-    par = bike.parameters[bike_name]
+    par = bike.parameters['Benchmark']
     parPure = bp.io.remove_uncertainties(par)
     return parPure
+
+    # generates nested dict with keys = parameter, and values = {column ID: data val}
+def genDataDic(constant, value, index, end):
+    data_dic = OrderedDict()
+    parPure = new_par(value)
+    for i in constant:     
+        for p in range(index,end):   
+            data_dic[pList[p]] = {i['id']:parPure.get(pList[p])}
+            index += 1
+            if index == 4:
+                break
+    return data_dic
+
+pList=['rF','mF','IFxx','IFyy','rR','mR','IRxx','IRyy','w','c','lam','g','xB','zB','mB','IBxx','IByy','IBzz','xH','zH','mH','IHxx','IHyy','IHzz','IHxz',]
 
 OPTIONS=['Benchmark',
          'Browser',
@@ -29,9 +45,31 @@ OPTIONS=['Benchmark',
          'Pista',
          'Rigid',
          'Silver',
-         'Tms',
          'Yellow',
-         'Yellowrev']    
+         'Yellowrev']
+
+WHEEL_COLUMNS=[{'name': 'Front Wheel', 'id': 'fW'},
+               {'name': 'Rear Wheel', 'id': 'rW'}]
+
+WHEEL_ROWS=['Radius',
+            'Mass',
+            'Moment Ixx',
+            'Moment Iyy']
+
+FRAME_COLUMNS=[{'name': 'General', 'id': 'bg'},
+               {'name': 'Frame', 'id': 'rF'},
+               {'name': 'Rider', 'id': 'rider'},
+               {'name': 'Rear Rack', 'id': 'rack'},
+               {'name': 'Front End', 'id': 'fE'},
+               {'name': 'Basket', 'id': 'basket'}]    
+
+FRAME_ROWS=['CoM X',
+            'CoM Y',
+            'Mass',
+            'Moment I11',
+            'Moment I22',
+            'Moment Izz',
+            'Principle Axis Angle']
 
 app = dash.Dash(__name__)
 
@@ -40,9 +78,9 @@ app.layout = html.Div([
             id='main-header'),
     dcc.Dropdown(id='bike-dropdown',
                  value='Benchmark',
-                 options=[{'label': i, 'value': i} for i in OPTIONS]),
-    tbl.DataTable(id='par-table',
-                  columns=[],
+                 options=[]),
+    tbl.DataTable(id='wheel-table',
+                  columns=WHEEL_COLUMNS,
                   data=[],      
                   style_cell={},
                   style_header={},
@@ -55,6 +93,14 @@ app.layout = html.Div([
                 id='dark-toggle',
                 type='button',
                 n_clicks=0),
+    html.Button('Update Dropdown Menu',
+                id='drop-button',
+                type='button',
+                n_clicks=0),
+    dcc.Input(placeholder='Save File As...',
+              value='',
+              id='save-input',
+              type='text'),
     html.Div(id='image-bin',
              children=[html.Img(src='',  
                                 alt='A plot revealing the general geometry, centers of mass and moments of inertia of the given bicycle system.',
@@ -64,14 +110,41 @@ app.layout = html.Div([
                                 id='eigen-plot')])
 ])
 
-    # Populates par-table data with parameters on new dropdown value/button press
-@app.callback(Output('par-table', 'data'), [Input('bike-dropdown', 'value'), Input('reset-button', 'n_clicks')])
-def populate_data(value, n_clicks):
-    bike = bp.Bicycle(value, pathToData=os.getcwd()+'\\data')
-    par = bike.parameters['Benchmark']
-    parPure = bp.io.remove_uncertainties(par)
-    return [parPure]
+    # Updates dropdown options with save-input value
+@app.callback(Output('bike-dropdown', 'options'), [Input('drop-button', 'n_clicks')], [State('save-input', 'value')])
+def update_dropdown(n_clicks, value):
+    if value == '':
+        return [{'label': i, 'value': i} for i in OPTIONS]
+    else:
+        OPTIONS.append(value)
+        return [{'label': i, 'value': i} for i in OPTIONS]
 
+    # Populates wheel-table parameter with data
+@app.callback(Output('wheel-table', 'data'), [Input('bike-dropdown', 'value'), Input('reset-button', 'n_clicks')])
+def populate_data(value, n_clicks):
+    dataDic = genDataDic(WHEEL_COLUMNS, value, 0, 8)
+    data = []
+    fW = []
+    rW = []
+    index = 0
+    for key in dataDic:
+        if index < 4:
+            fW.append(dataDic.get(key)) 
+        if index >= 4:
+            rW.append(dataDic.get(key))
+        index += 1 
+    for c, d in zip(fW, rW):
+        zipped = {}
+        zipped.update(c)
+        zipped.update(d)
+        data.append(zipped)
+    print(dataDic)
+    print(rW+fW)
+    print(data)
+    return data
+    
+
+'''
     # Populates par-table columns with proper values
 @app.callback(Output('par-table', 'columns'), [Input('bike-dropdown', 'value')])
 def populate_columns(value):
@@ -79,7 +152,7 @@ def populate_columns(value):
     par = bike.parameters['Benchmark']
     columns = [{'name': i, 'id': i} for i in par]
     return columns
-    
+'''    
     # Updates geometry-plot path with Dropdown value
 @app.callback(Output('geometry-plot', 'src'), [Input('bike-dropdown', 'value')])
 def reveal_geo_plot(value):
@@ -99,7 +172,7 @@ def reveal_geo_plot(value):
         return '/assets/eigen-plots/user-bikes/'+image
 
     # Toggles dark mode for cells of DataTable
-@app.callback(Output('par-table', 'style_cell'), [Input('dark-toggle', 'n_clicks')])
+@app.callback(Output('wheel-table', 'style_cell'), [Input('dark-toggle', 'n_clicks')])
 def cell_toggle(n_clicks):
     if n_clicks%2 == 1:
         return {'minWidth': '50px', 'width': '50px', 'maxWidth': '50px', 'backgroundColor': 'rgb(50, 50, 50)', 'color': 'white'}
@@ -107,8 +180,8 @@ def cell_toggle(n_clicks):
         return {'minWidth': '50px', 'width': '50px', 'maxWidth': '50px', 'backgroundColor': 'rgb(255, 255, 255)', 'color': 'black'}
 
     # Toggles dark mode for header of DataTable
-@app.callback(Output('par-table', 'style_header'), [Input('dark-toggle', 'n_clicks')])
-def cell_toggle(n_clicks):
+@app.callback(Output('wheel-table', 'style_header'), [Input('dark-toggle', 'n_clicks')])
+def header_toggle(n_clicks):
     if n_clicks%2 == 1:      
         return {'textAlign': 'center', 'backgroundColor': 'rgb(30, 30, 30)'}
     else:
