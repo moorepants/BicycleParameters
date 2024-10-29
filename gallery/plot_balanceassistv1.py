88d353860a4b557da7fad6a7f5025818201f2d8a
Jason K. Moore <moorepants@gmail.com>
2024-10-29 06:47:31 +0100

    Added control to balance assist example.

--- gallery/plot_balanceassistv1.py
+++ gallery/plot_balanceassistv1.py
@@ -12,7 +12,7 @@ Balance Assist V1
 import numpy as np
 import bicycleparameters as bp
 from bicycleparameters.parameter_sets import Meijaard2007ParameterSet
-from bicycleparameters.models import Meijaard2007Model
+from bicycleparameters.models import Meijaard2007WithFeedbackModel
 
 data_dir = "../data"
 
@@ -20,12 +20,16 @@ bicycle = bp.Bicycle("Balanceassistv1", pathToData=data_dir)
 par = bp.io.remove_uncertainties(bicycle.parameters['Benchmark'])
 par['v'] = 1.0
 par_set = Meijaard2007ParameterSet(par, False)
+par_set
+
+# %%
 par_set.plot_all()
 
 # %%
-model = Meijaard2007Model(par_set)
+model = Meijaard2007WithFeedbackModel(par_set)
 v = np.linspace(0.0, 10.0, num=401)
-model.plot_eigenvalue_parts(v=v)
+ax = model.plot_eigenvalue_parts(v=v)
+ax.set_ylim((-10.0, 10.0))
 
 # %%
 bicycle.add_rider('Jason', reCalc=True)
@@ -35,5 +39,30 @@ bicycle.plot_bicycle_geometry(inertiaEllipse=False)
 par = bp.io.remove_uncertainties(bicycle.parameters['Benchmark'])
 par['v'] = 1.0
 par_set = Meijaard2007ParameterSet(par, True)
-model = Meijaard2007Model(par_set)
-model.plot_eigenvalue_parts(v=v)
+par_set.plot_all()
+
+# %%
+model = Meijaard2007WithFeedbackModel(par_set)
+ax = model.plot_eigenvalue_parts(v=v)
+ax.set_ylim((-10.0, 10.0))
+
+# %%
+# :math:`\kappa*(v_\textrm{max} - v)`
+vmin, vmin_idx = 1.5, np.argmin(np.abs(v - 1.5))
+vmax, vmax_idx = 4.7, np.argmin(np.abs(v - 4.7))
+static_gain = 10.0
+kphidots = -static_gain*(vmax - v)
+kphidots[:vmin_idx] = -static_gain*(vmax - vmin)/vmin*v[:vmin_idx]
+kphidots[vmax_idx:] = 0.0
+model.plot_gains(v=v, kTdel_phid=kphidots)
+
+# %%
+ax = model.plot_eigenvalue_parts(v=v, kTdel_phid=kphidots)
+ax.set_ylim((-10.0, 10.0))
+
+# %%
+times = np.linspace(0.0, 2.0, num=201)
+x0 = np.array([0.0, 0.0, 0.5, 0.0])
+kphidot = kphidots[np.argmin(np.abs(v - 3.0))]
+ax = model.plot_simulation(times, x0, v=3.0)
+ax = model.plot_simulation(times, x0, axes=ax, v=3.0, kTdel_phid=kphidot)
